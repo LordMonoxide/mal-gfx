933f783e76ea9b1b4ac7c2aa731f2811e10d407a
Corey Frenette <corey@narwhunderful.com>
2014-10-13 18:46:00 -0300

    GL3 skeleton

--- mal-gfx/src/main/java/malachite/gfx/WindowBuilder.java
+++ mal-gfx/src/main/java/malachite/gfx/WindowBuilder.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 public class WindowBuilder {
   public static void main(String[] args) {
     Window window = new WindowBuilder()
+      .registerContext(malachite.gfx.gl32.ContextBuilder.class)
       .registerContext(malachite.gfx.gl21.ContextBuilder.class)
       .setTitle("Test").build();
   }
---
+++ mal-gfx/src/main/java/malachite/gfx/gl32/Context.java
@@ -0,0 +1,17 @@
+package malachite.gfx.gl32;
+
+import malachite.gfx.AbstractContext;
+
+public class Context extends AbstractContext {
+  Context(int fps) {
+    super(fps);
+  }
+  
+  @Override protected void updateSize() {
+    
+  }
+  
+  @Override protected void cleanup() {
+    
+  }
+}
