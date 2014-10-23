e18eae8ba0ba75c8d1d05d7b2ab31c219d276e87
Corey Frenette <corey@cardless.me>
2014-10-22 21:41:33 -0300

    Allow unindexed Drawables

--- mal-gfx/src/main/java/malachite/gfx/DrawableVBOIndexed.java
+++ mal-gfx/src/main/java/malachite/gfx/DrawableVBOIndexed.java
@@ -30,6 +30,7 @@ public class DrawableVBOIndexed implements Drawable {
   private int _vaID;
   private int _vbID;
   private int _ibID;
+  private int _vertices;
   private int _indices;
   
   DrawableVBOIndexed() {
@@ -59,7 +60,8 @@ public class DrawableVBOIndexed implements Drawable {
   }
   
   private void updateVBO(FloatBuffer vertices, ByteBuffer indices) {
-    _indices = indices.remaining();
+    _vertices = vertices.remaining();
+    _indices = indices != null ? indices.remaining() : 0;
     
     _vaID = GL30.glGenVertexArrays();
     GL30.glBindVertexArray(_vaID);
@@ -72,15 +74,21 @@ public class DrawableVBOIndexed implements Drawable {
     
     GL30.glBindVertexArray(0);
     
-    _ibID = GL15.glGenBuffers();
-    GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, _ibID);
-    GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indices, GL15.GL_STATIC_DRAW);
-    GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
+    if(_indices != 0) {
+      _ibID = GL15.glGenBuffers();
+      GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, _ibID);
+      GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indices, GL15.GL_STATIC_DRAW);
+      GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
+    }
   }
   
   @Override public void destroy() {
     GL15.glDeleteBuffers(_vbID);
-    GL15.glDeleteBuffers(_ibID);
+    
+    if(_indices != 0) {
+      GL15.glDeleteBuffers(_ibID);
+    }
+    
     GL30.glDeleteVertexArrays(_vaID);
   }
   
@@ -88,11 +96,14 @@ public class DrawableVBOIndexed implements Drawable {
     GL30.glBindVertexArray(_vaID);
     GL20.glEnableVertexAttribArray(0);
     
-    GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, _ibID);
+    if(_indices != 0) {
+      GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, _ibID);
+      GL11.glDrawElements(GL11.GL_TRIANGLES, _indices, GL11.GL_UNSIGNED_BYTE, 0);
+      GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
+    } else {
+      GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, _vertices);
+    }
     
-    GL11.glDrawElements(GL11.GL_TRIANGLES, _indices, GL11.GL_UNSIGNED_BYTE, 0);
-    
-    GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
     GL20.glDisableVertexAttribArray(0);
     GL30.glBindVertexArray(0);
   }
