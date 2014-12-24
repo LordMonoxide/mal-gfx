ab9cb1274e0c11b997e17667fce96bc5c842ae3b
Corey Frenette <corey@cardless.me>
2014-12-23 20:42:09 -0400

    Check for GL30 framebuffer errors

--- mal-gfx/src/main/java/mal/gfx/textures/GL30RenderTarget.java
+++ mal-gfx/src/main/java/mal/gfx/textures/GL30RenderTarget.java
@@ -2,6 +2,7 @@ package mal.gfx.textures;
 
 import mal.gfx.Context;
 
+import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 
 public class GL30RenderTarget extends RenderTarget {
@@ -10,6 +11,11 @@ public class GL30RenderTarget extends RenderTarget {
     
     use();
     GL30.glFramebufferTexture2D(renderID, GL30.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT0, id, 0);
+    GL20.glDrawBuffers(GL30.GL_COLOR_ATTACHMENT0);
+    
+    if(GL30.glCheckFramebufferStatus(GL30.GL_FRAMEBUFFER) != GL30.GL_FRAMEBUFFER_COMPLETE) {
+      System.err.println("Framebuffer incomplete!\n" + GL30.glCheckFramebufferStatus(GL30.GL_FRAMEBUFFER));
+    }
   }
   
   @Override protected int generateRenderID() {
